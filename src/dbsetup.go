297ea99dac58af3565aa7b1dab39c4153bf32ac5
rishavchakra <rishavchakra@gmail.com>
2024-10-10 02:26:01 -0400

    Fix DB connection

--- src/dbsetup.go
+++ src/dbsetup.go
@@ -11,7 +11,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USERNAME"),
 		os.Getenv("DB_PASSWORD"),
